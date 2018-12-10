97bed5dc6775382a99b47e0d7c4a526c7ba9d9c5
Kiel Lewis <kiel.lewis@gmail.com>
2018-12-10 18:24:15 -0500

    Proj 2 meets expectations
    Project completed. Feel my appendPageLinks could be refactored somehow but not sure how to go about it.

--- Starter files/List Pagination & Filtering/js/script.js
+++ Starter files/List Pagination & Filtering/js/script.js
@@ -5,34 +5,10 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 const mainDiv = document.querySelector('.page');
 const listItems = document.getElementsByClassName('student-item')
 const studentCollection = document.getElementsByTagName('H3');
 const buttons = document.getElementsByTagName('A');
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
 
 showPage = (list, page) => {
    // hide all items
@@ -46,12 +22,13 @@ showPage = (list, page) => {
       }    
    }
 }
+
 showPage(listItems, 1);
 
 appendPageLinks = () => { 
    // create parents
    const div = document.createElement('div');
-   div.className = 'pagination'; // assign class to display buttons
+   div.className = 'pagination'; // assign class to display below
    mainDiv.appendChild(div);
 
    const ul = document.createElement('ul');
@@ -62,19 +39,25 @@ appendPageLinks = () => {
       const li = document.createElement('li');
       ul.appendChild(li);
       const link = document.createElement('a');
-      link.setAttribute('href', '#');
+      link.setAttribute('href', '#'); // <--- adapted from https://www.peterbe.com/plog/createelement-a
       link.textContent = (i / 10) + 1; // button numbers
       li.appendChild(link); 
    }
    ul.firstElementChild.firstElementChild.className = 'active';
 
-  // button functionality
+  // page button functionality
   for ( let i = 0; i < buttons.length; i++){
    buttons[i].addEventListener( 'click', function(){
       showPage(listItems,buttons[i].textContent)
+      for ( let i = 0; i < buttons.length; i++){
+         buttons[i].className = ''; // must set all classes to empty before adding to selected page
+      }
+      if(buttons[i].className === ''){
+         buttons[i].className = 'active';
+      }
    })
 }
-  
+
 }
 appendPageLinks();
 
