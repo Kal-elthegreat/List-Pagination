58fc9a5b960dab4140602a34bfee074f6c0de84a
Kiel Lewis <kiel.lewis@mailchimp.com>
2020-01-18 22:47:37 -0500

    refactored how I select the page

--- js/script.js
+++ js/script.js
@@ -6,34 +6,32 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 const listItemCollection = document.getElementsByClassName('student-item')
-
-const studentCollection = document.getElementsByTagName('H3');
-const buttons = document.getElementsByTagName('A');
-
 showPage = (list, page) => {
    [...list].forEach( item => item.style.display = 'none');
-
+   
    [...list].forEach( (item,index) => {
       if ( index < (page * 10) && index >= (page * 10) - 10){
          item.style.display = '';
       }
    });
 }
-
 showPage(listItemCollection, 1);
 
-appendPageLinks = () => { 
-   //debugger;
+/*
+* Create & append parent divs to the main page
+*/
+const studentCollection = document.getElementsByTagName('H3');
+appendPageLinks = () => {
    // grab main div and create div to hold link buttons 7 append
    const mainDiv = document.querySelector('.page');
    const div = document.createElement('div');
    div.className = 'pagination';
    mainDiv.appendChild(div);
-
+   
    const ul = document.createElement('ul');
    div.appendChild(ul);
-
-   // create new elements & append to parents 
+   
+   // create links & append to parents 
    for (let i = 0; i < studentCollection.length; i += 10){ // increment by 10 bc 10 ppl are shown on each page
       const li = document.createElement('li');
       ul.appendChild(li);
@@ -43,27 +41,21 @@ appendPageLinks = () => {
       li.appendChild(link); 
    }
    ul.firstElementChild.firstElementChild.className = 'active';
-
-  // page button functionality
-  for ( let i = 0; i < buttons.length; i++){
-   buttons[i].addEventListener( 'click', function(){
-      showPage(listItemCollection,buttons[i].textContent)
-      for ( let i = 0; i < buttons.length; i++){
-         buttons[i].className = ''; // must set all classes to empty before adding to selected page
-      }
-      if(buttons[i].className === ''){
-         buttons[i].className = 'active';
-      }
-   })
+   selectPage();
 }
 
+/*
+* Adds event listener to each page link
+*/
+selectPage = () => {
+   const buttons = document.getElementsByTagName("A");
+   [...buttons].forEach(function(button){
+      button.addEventListener("click", function () {
+         showPage(listItemCollection, button.textContent);
+         [...buttons].forEach(button => button.className = '');
+         button.className = 'active';
+      });
+   });
 }
+
 appendPageLinks();
-
-/********* IGNORE CODE BELOW THIS LINE.  *************/
-// display range of 10
-//  range = (start, stop) => {
-//    for( let i = start; i < stop; i ++) { 
-//    listItemCollection[i].style.display = '';
-//    }
-// }
